0ef1f4d236fa59bcc18d038e1b3d16167dfb8d11
sunpx3 <tbxzfw@163.com>
2017-07-16 19:58:52 +0800

    加入代码生成器.

--- src/test/java/com/zg/generator/PsychGenerator.java
+++ src/test/java/com/zg/generator/PsychGenerator.java
@@ -7,27 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-
-import com.zg.core.generator.api.MyBatisGenerator;
-import com.zg.core.generator.config.Configuration;
-import com.zg.core.generator.config.xml.ConfigurationParser;
-import com.zg.core.generator.exception.InvalidConfigurationException;
-import com.zg.core.generator.exception.XMLParserException;
-import com.zg.core.generator.internal.DefaultShellCallback;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
 
 /**
  * 龙果教程-Spring Boot全集
  * 
  * @author wujing
  */
-public class RoncooSpringBoot {
-	private static Logger logger = Logger.getLogger(RoncooSpringBoot.class);
+public class PsychGenerator {
+	private static Logger logger = Logger.getLogger(PsychGenerator.class);
 
 	public static void main(String[] args) throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
 		logger.info("Start generator ...");
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true; // 是否覆盖原来的文件
-		File configFile = new File("H:/spring-boot-list/psych/src/test/resources/psych-spring-boot.xml");
+		File configFile = new File("H:\\spring-boot-list\\psych\\src\\test\\resources\\psych-spring-boot.xml");
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
