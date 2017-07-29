3b00496d36ff9ab0157effdea0837f897b560105
sunpx3 <tbxzfw@163.com>
2017-07-29 23:12:33 +0800

    文件类型表。

--- src/main/java/com/zg/core/controller/FileUploadController.java
+++ src/main/java/com/zg/core/controller/FileUploadController.java
@@ -1,9 +1,14 @@
 package com.zg.core.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 文件上传
+ * 
  * @author sunpx
  *
  */
@@ -20,26 +26,41 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/fileupload")
 public class FileUploadController {
 
+	/**
+	 * 使用value注解可以读取application.properties文件内容。
+	 */
+	@Value("${web.upload-path}")
+	private String uploadPath;
+
 	@RequestMapping(params = "uploadFile")
-	public ModelAndView fileUploadIndex(){
-		
+	public ModelAndView fileUploadIndex() {
+
 		return new ModelAndView("admin/fileupload");
 	}
-	
+
 	@RequestMapping(params = "uploadFiles")
-	public ModelAndView uploadFiles(MultipartRequest  multipartRequest ,
-			HttpServletRequest request) {
-		
-		//获取多个file  
-		for (Iterator it = multipartRequest.getFileNames(); it.hasNext();) {  
-		    MultipartFile imgFile = multipartRequest.getFile(it.next().toString());  
-		    if (imgFile.getOriginalFilename().length() > 0) {  
-		        String fileName = imgFile.getOriginalFilename();  
-		        System.out.println(fileName);
-		    }  
-		}  
-		
+	public ModelAndView uploadFiles(MultipartRequest multipartRequest, HttpServletRequest request) {
+
+		try {
+			// 获取多个file
+			for (Iterator it = multipartRequest.getFileNames(); it.hasNext();) {
+				MultipartFile imgFile = multipartRequest.getFile(it.next().toString());
+				if (imgFile.getOriginalFilename().length() > 0) {
+					String fileName = imgFile.getOriginalFilename();
+					// System.out.println(fileName);
+					FileOutputStream os = new FileOutputStream(new File(uploadPath + fileName));
+					BufferedOutputStream buffStream = new BufferedOutputStream(os);
+					buffStream.write(imgFile.getBytes());
+					buffStream.close();
+					os.close();
+					System.out.println("文件" + fileName + "上传成功！");
+				}
+			}
+		} catch (IOException e) {
+		} catch (Exception e) {
+		}
+
 		return new ModelAndView("admin/fileupload");
-		
+
 	}
 }
