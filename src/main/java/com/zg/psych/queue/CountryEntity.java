1760982ca8094abbb2d56959ece49b212bc35a7a
sunpx3 <tbxzfw@163.com>
2017-08-06 15:18:34 +0800

    测试生产者消费者模型

---
+++ src/main/java/com/zg/psych/queue/CountryEntity.java
@@ -0,0 +1,35 @@
+package com.zg.psych.queue;
+
+/**
+ * Created by sunpx on 2017/8/6.
+ */
+public class CountryEntity {
+
+    private String id;
+    private String countName;
+    private String countCode;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCountName() {
+        return countName;
+    }
+
+    public void setCountName(String countName) {
+        this.countName = countName;
+    }
+
+    public String getCountCode() {
+        return countCode;
+    }
+
+    public void setCountCode(String countCode) {
+        this.countCode = countCode;
+    }
+}
